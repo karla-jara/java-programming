9f6a720e8a028ad12ed0062a0d23c046de0d3345
Karla Jara <jara.karla.43@gmail.com>
2021-09-15 23:58:16 -0500

    java practice

--- src/Hello.java
+++ src/Hello.java
@@ -1,3 +1,6 @@
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class Hello {
     public static void main(String[] args){
         System.out.println("Hello, Karla");
@@ -5,4 +8,27 @@ public class Hello {
         int myFirstNumber = 5;
         System.out.println("myFirstNumber = " + myFirstNumber);
     }
-}
+
+//    Create a function that converts 12-hour time to 24-hour time or vice versa. Return the output as a string.
+
+
+
+    public class Challenge {
+        public static String convertTime(String time) {
+            try {
+                time = time.replace("a", "A").replace("p", "P").replace("m", "M");
+                System.out.println(time);
+                DateTimeFormatter twelve = DateTimeFormatter.ofPattern("h:mm a");
+                DateTimeFormatter twentyFour = DateTimeFormatter.ofPattern("H:mm");
+                if (time.matches(".*[AP]M")) {
+                    LocalTime pt = LocalTime.parse(time, twelve);
+                    return pt.format(twentyFour);
+                }
+                LocalTime pt = LocalTime.parse(time, twentyFour);
+                return pt.format(twelve).replace("AM", "am").replace("PM", "pm");
+            }
+            catch (Exception e) {
+                return time;
+            }
+        }
+}}
