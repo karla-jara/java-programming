dfb5e45236f78a17a2f50a9c9d39d2ef77bc4d66
Karla Jara <jara.karla.43@gmail.com>
2021-09-20 23:44:07 -0500

    java practice exercise

--- src/UserInput.java
+++ src/UserInput.java
@@ -31,4 +31,12 @@ public class UserInput {
 
 
 
+
+
+
+
+    public static int addition(int num) {
+        //Create a function that takes a number as an argument, increments the number by +1 and returns the result.
+        return num + 1;
+    }
 }
